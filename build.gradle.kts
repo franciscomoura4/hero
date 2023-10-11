c5218ff5b807607e48a419428a3e3ac1cc122775
franciscomoura4 <chicomoura112@icloud.com>
2023-10-11 20:42:25 +0100

    segunda versao

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     id("java")
+    id("application")
 }
 
-group = "com.yourusername.hero"
+application{
+    mainClass.set("Application")
+}
+
+group = "com.francisco4.hero"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,8 +17,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.googlecode.lanterna:lanterna:3.1.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+
